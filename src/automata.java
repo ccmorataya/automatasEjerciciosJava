f6702b46a0c3643df7900c86443cfe5b2edf1573
Cristian Morataya <cris.morataya@gmail.com>
2016-10-23 17:44:26 -0600

    First aproach to the AFD:: a+

--- src/automata.java
+++ src/automata.java
@@ -20,4 +20,19 @@ class automata {
         }
         return isValid;
     }
+
+    static boolean afdDos(String cadena){
+        boolean isValid;
+        int status = 0;
+        for (int i = 0; i < cadena.length(); i++) {
+            Character c = cadena.charAt(i);
+            if (status == 0) {
+                if (c == 'a') {
+                    status = 1;
+                }
+            }
+        }
+        isValid = status == 1;
+        return isValid;
+    }
 }
