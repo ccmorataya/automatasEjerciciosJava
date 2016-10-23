0b349022aacd400b93696e7089ac9007444470c4
Cristian Morataya <cris.morataya@gmail.com>
2016-10-23 13:43:15 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String cadena = "a";
+        automata(cadena);
+    }
+    static boolean automata(String cadena){
+        for (int i = 0; i < cadena.length(); i++)
+        {
+
+        }
+            return false;
+
+    }
+}
