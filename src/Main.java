30e5b7da6868fc71444946aefac6a0780c639d5f
Cristian Morataya <cris.morataya@gmail.com>
2016-10-23 14:12:26 -0600

    Using the scanner variable to get the user input

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String cadena = "a";
+        String cadena;
+        Scanner datos = new Scanner(System.in);
+        cadena = datos.nextLine();
         automata(cadena);
     }
     static boolean automata(String cadena){
         for (int i = 0; i < cadena.length(); i++)
         {
-
+            Character c = cadena.charAt(i);
+            System.out.println(c);
         }
             return false;
 
